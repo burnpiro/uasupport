25a2a8d565733aa67062c6df1fb627a9c0859f03
burnpiro <kemalpiro@gmail.com>
2022-03-14 02:02:53 +0100

    Add firebase/analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getStorage, connectStorageEmulator } from 'firebase/storage';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
@@ -10,12 +11,13 @@ import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnIHO0seWnkqZ15xBCIMsE4xLXsVg-Lwk",
-  authDomain: "ukraina-32e0b.firebaseapp.com",
-  projectId: "ukraina-32e0b",
-  storageBucket: "ukraina-32e0b.appspot.com",
-  messagingSenderId: "988341542249",
-  appId: "1:988341542249:web:35f1ad49d1d2d86b36f1f8"
+  apiKey: 'AIzaSyCnIHO0seWnkqZ15xBCIMsE4xLXsVg-Lwk',
+  authDomain: 'ukraina-32e0b.firebaseapp.com',
+  projectId: 'ukraina-32e0b',
+  storageBucket: 'ukraina-32e0b.appspot.com',
+  messagingSenderId: '988341542249',
+  appId: '1:988341542249:web:35f1ad49d1d2d86b36f1f8',
+  measurementId: 'G-D16EDV9NZC'
 };
 
 // Initialize Firebase
@@ -25,6 +27,7 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const functions = getFunctions(app);
+export const analytics = getAnalytics(app);
 
 // localhost firebase (it works only on emulated env)
 // connectFirestoreEmulator(db, 'localhost', 8081);
